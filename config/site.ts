407eacc025ab548373ce08552f76cec0d959e7c7
Oscar19260 <oscar19260@gmail.com>
2023-11-15 13:10:55 +0100

    Wacamole website is back!

---
+++ config/site.ts
@@ -0,0 +1,54 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: "Wacamole",
+	description: "Wacamole website",
+	navItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+		{
+			label: "Pricing",
+			href: "/",
+		},
+		{
+			label: "Blog",
+			href: "/",
+		},
+		{
+			label: "About",
+			href: "/",
+    	}
+	],
+	navMenuItems: [
+		{
+			label: "Profile",
+			href: "/",
+		},
+		{
+			label: "Dashboard",
+			href: "/",
+		},
+		{
+			label: "Team",
+			href: "/",
+		},
+		{
+			label: "Settings",
+			href: "/",
+		},
+		{
+			label: "Help & Feedback",
+			href: "/",
+		},
+		{
+			label: "Logout",
+			href: "/",
+		},
+	],
+	links: {
+		twitter: "/",
+		instagram: "/",
+	},
+};
